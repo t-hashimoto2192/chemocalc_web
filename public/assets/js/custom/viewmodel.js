6abae6cdf162bb13363ed0cff35c6647dca8a25a
DESKTOP-B89OIF4\user <t-hashimoto@chancelab.jp>
2018-05-10 23:46:04 +0900

    viewmodelのthisをselfに変更

--- public/assets/js/custom/viewmodel.js
+++ public/assets/js/custom/viewmodel.js
@@ -1,13 +1,15 @@
 /* knockout.js ViewModel定義 */
 function settingViewModel() {
 
+    var self = this;
+
     /* 体表面積計算フォーム */
     // 身長
-    this.stature = ko.observable('');
+    self.stature = ko.observable('');
     // 体重
-    this.weight = ko.observable('');
+    self.weight = ko.observable('');
     // 体表面積
-    this.bsa = ko.computed(function () {
+    self.bsa = ko.computed(function () {
         var sVal = this.stature();
         var wVal = this.weight();
         var bsaVal = calcBsa(sVal, wVal);
@@ -15,7 +17,7 @@ function settingViewModel() {
     }, this);
 
     /* サイドメニューのクリックイベント */
-    this.clickMenu = function (data, event) {
+    self.clickMenu = function (data, event) {
         changeRegimen(event.target);
     };
 
@@ -27,23 +29,26 @@ function settingViewModel() {
     /* ★☆★ 療法別画面 ★☆★ */
 
     /* FEC */
-    this.fec_fu_val = ko.observable('');
+    self.fec_fu_val = ko.observable('');
 
-    this.fec_cpa_val = ko.observable('');
-    this.fec_cpa_ref = ko.observable('');
+    self.fec_cpa_val = ko.observable('');
+    self.fec_cpa_ref = ko.observable('');
 
     /* DOC */
-    this.docDosageLnk = ko.observable('');
-    this.docDosageLbl = ko.computed(function () {
+    self.docDosageLnk = ko.observable('');
+    self.docDosageLbl = ko.computed(function () {
         // 参考使用量
-        return calcDosageVal(this.bsa(), this.docDosageLnk());
-    }, this);
-    this.docDosageTxt = ko.observable('');
-    this.docPrice = ko.observable('');
-    this.docTotalPrice = ko.observable('');
-    this.docTotalPrice30per = ko.observable('');
-    this.docTotalPrice20per = ko.observable('');
-    this.docTotalPrice10per = ko.observable('');
+        return calcDosageVal(self.bsa(), self.docDosageLnk());
+    });
+    self.docDosageTxt = ko.observable('');
+    
+    self.docStandardArray = ko.observableArray();
+    
+    self.docPrice = ko.observable('');
+    self.docTotalPrice = ko.observable('');
+    self.docTotalPrice30per = ko.observable('');
+    self.docTotalPrice20per = ko.observable('');
+    self.docTotalPrice10per = ko.observable('');
 }
 ;
 
